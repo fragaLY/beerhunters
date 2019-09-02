a201dfb5c665ff85adc9d836b321daeea0641bd5
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-09-02 17:47:10 +0300

    Beerhunters

--- script/sopcast.js
+++ script/sopcast.js
@@ -3,6 +3,7 @@ const SOPCAST_URL = 'http://www.sopcast.com/channel/getch.html?';
 function sopcast() {
     let groupId = $('#groupId').val();
     let channelId = $('#channelId').val();
-    document.getElementById('sopchannel')
-        .setAttribute("src", SOPCAST_URL.concat("groupId=").concat(groupId).concat("&channelId=").concat(channelId))
+    let sopcastElement = document.getElementById('sopchannel');
+    sopcastElement.setAttribute("src", SOPCAST_URL.concat("groupId=").concat(groupId).concat("&channelId=").concat(channelId));
+    sopcastElement.attr("hidden", false);
 }
