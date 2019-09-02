3afdd9859fe88dafaf245447cdc2e3ed45fbd034
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-09-02 14:04:35 +0300

    Beerhunters

---
+++ script/ace-stream.js
@@ -0,0 +1,12 @@
+function acestream(contentId) {
+    let player = null;
+    AceStream.createPlayer("#ace-stream-player", {
+        onLoad: function (playerObj) {
+            player = playerObj;
+            player.loadContentId(contentId);
+        },
+        onError: function (err) {
+            console.log("init: error: " + err);
+        }
+    })
+}
