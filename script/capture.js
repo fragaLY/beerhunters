9f9c54b71aebbf3665bb813e77280eb60c0c6590
Vadzim Kavalkou <Vadzim_Kavalkou@epam.com>
2019-09-02 13:34:50 +0300

    Beerhunters

--- script/capture.js
+++ script/capture.js
@@ -1,7 +1,16 @@
 function capture() {
-    const URL_BT5 = "https://www.tvr.by/televidenie/belarus-5/";
-    const URL_BT5_INTERNET = "https://www.tvr.by/televidenie/belarus-5-internet/";
+    const URL_BT5 = "https://cors-anywhere.herokuapp.com/http://www.tvr.by/televidenie/belarus-5/";
+    const URL_BT5_INTERNET = "https://cors-anywhere.herokuapp.com/http://www.tvr.by/televidenie/belarus-5-internet/";
 
     axios.get(URL_BT5).then(data => console.log(data)).catch(err => console.log(err));
-    axios.get(URL_BT5_INTERNET).then(data => console.log(data)).catch(err => console.log(err))
+    axios.get(URL_BT5).then((response) => {
+        if (response.status === 200) {
+            const htmlTxt = response.data;
+            const el = document.createElement( 'html' );
+            el.innerHTML = htmlTxt;
+
+            let elementsByTagName = el.getElementsByClassName( 'uppod-control_start' );
+            console.log(elementsByTagName)
+        }
+    }).catch(err => console.log(err));
 }
